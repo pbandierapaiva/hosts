bd08df6a2c2589c6954bb3dc3ab5cd1eef7ec698
pbandierapaiva <paiva@unifesp.br>
2021-12-20 15:39:15 -0300

    implementando side panel

--- hostapi/static/dcp.js
+++ hostapi/static/dcp.js
@@ -1,19 +1,29 @@
 
 function carregaHosts() {
   const xhttp = new XMLHttpRequest();
-  xhttp.onload = function() {
+  xhttp.onload = onLoadHosts;
+  xhttp.open("GET", "/hosts");	
+  xhttp.send();
+}
 
-    li = document.createElement("ul")
-    li.className="w3-ul w3-hoverable w3-border"
+function onLoadHosts(){
+
+//    li = document.createElement("ul")
+//    li.className="w3-ul w3-hoverable w3-border"
+    li = document.createElement("a")
+
     
     for (i of JSON.parse(this.responseText)) {
-    	item = document.createElement("li")
+    	item = document.createElement("a")
+    	item.className="w3-bar-item w3-button"
     	//celula = document.createElement("div")
     	estadoHost = document.createElement("span")
     	estadoHost.innerHTML = "&nbsp;"
-    	nomehost = document.createElement("a")
-    	nomehost.href = "http://"+i.ip
-    	nomehost.innerHTML = i.nome
+    	//nomehost = document.createElement("a")
+    	//nomehost.className = "w3-button w3-black"
+    	item.href = "http://"+i.ip
+    	item.target="#infoarea"
+    	item.innerHTML = i.nome
       	if( i.estado==0 ) {
     		estadoHost.className="w3-badge w3-right w3-margin-right w3-grey"
     		}
@@ -26,12 +36,10 @@ function carregaHosts() {
     			}
     		}
     	item.appendChild(estadoHost)
-    	item.appendChild(nomehost)         //document.createTextNode(i.nome))
+    	//item.appendChild(nomehost)         //document.createTextNode(i.nome))
     	//item.innerHTML =  i.nome 
-    	li.appendChild ( item )
-    	document.getElementById("listahost").appendChild(li)
+    	//li.appendChild ( item )
+    	document.getElementById("listahost").appendChild(item)
     }
   }
-  xhttp.open("GET", "/hosts");	
-  xhttp.send();
-}
+
