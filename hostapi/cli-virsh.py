c9310c9ef0e397481c3d5920bfa284239a27af63
pbandierapaiva <paiva@unifesp.br>
2022-03-03 18:17:53 -0300

    Todos MACs de VMs

--- hostapi/cli-virsh.py
+++ hostapi/cli-virsh.py
@@ -7,31 +7,69 @@ import sys
 # import xml
 import xml.etree.ElementTree as ET
 
-auth = [[libvirt.VIR_CRED_AUTHNAME, libvirt.VIR_CRED_PASSPHRASE],'root','3e4rfgt5']
+import mariadb
+from conexao import conexao, rootpw
+
+class DB:
+	def __init__(self):
+		self.con= mariadb.connect(**conexao)
+		self.cursor= self.con.cursor(dictionary=True)
+	def commit(self):
+		self.con.commit()
+		
+def allHosts():
+	db = DB()
+
+	# somente Hosts que não são VMs 'V'
+	# db.cursor.execute("Select id,nome,estado,tipo,cpu,n,mem from maq where tipo!='V'")
+	db.cursor.execute("Select id,nome,estado,tipo,cpu,n,mem from maq where tipo='H'")
+	todoshostsdb = db.cursor.fetchall()
+
+	contavm = 0
+
+	d = []
+	for li in todoshostsdb:
+		print("\n",li)
+		db.cursor.execute("SELECT ip,rede FROM netdev WHERE maq='%s'"%li['id'])
+		allIf =  db.cursor.fetchall()
+		ifes={}
+		for iface in allIf:
+			ifes[iface['rede']]=iface['ip']
+			
+		print("IPMI: ",ifes['ipmi'])
+		for rede in ifes:
+			if rede!='ipmi':
+				print(ifes[rede])
+				if( fetchVMs( ifes[rede]) ): break
 
 
-try:
-	conn = libvirt.open('qemu+ssh://root@10.90.0.213/system') #,auth,0)
-except libvirt.libvirtError as e:
-	print(repr(e),file=sys.stderr)
-	exit(1)
+def fetchVMs(ip):
+	status=False
 
-for d in conn.listAllDomains():  #libvirt.VIR_CONNECT_LIST_DOMAINS_ACTIVE):
-	print(d.name(), d.isActive(), d.isPersistent())
-	
-	raiz= ET.fromstring(d.XMLDesc())
-	# print(d.XMLDesc())
-	# for x in raiz.findall('mac'):
-	# 	print("XXXXX",x.attribute())
+	try:
+		conn = libvirt.open(f'qemu+ssh://root@{ip}/system') #,auth,0)
+	except libvirt.libvirtError as e:
+		print(repr(e),file=sys.stderr)
+		return status
+	status = True
+	for d in conn.listAllDomains():  #libvirt.VIR_CONNECT_LIST_DOMAINS_ACTIVE):
+		print(d.name(), d.isActive(), d.isPersistent())
 		
-	for el in raiz:
-		if el.tag=='devices':
-			for el2 in el:
-				if el2.tag=='interface':
-					for el3 in el2:
-						if el3.tag=='mac':
-							print("\t",el3.attrib['address'])
-	
+		raiz= ET.fromstring(d.XMLDesc())
+		# print(d.XMLDesc())
+		# for x in raiz.findall('mac'):
+		# 	print("XXXXX",x.attribute())
+		for el in raiz:
+			if el.tag=='devices':
+				for el2 in el:
+					if el2.tag=='interface':
+						for el3 in el2:
+							if el3.tag=='mac':
+								print("\t",el3.attrib['address'])
+							if el3.tag=='vlan':
+								print("\t",el3.text)
+	return status
 		
-		
+allHosts()		
+
 		
